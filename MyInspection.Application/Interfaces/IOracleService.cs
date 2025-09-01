8bf87aba56a677a8f44116ec616bdccde90242a0
Lasantha Karunarathne <lasantha.20223087@iit.ac.lk>
2025-09-01 11:01:36 +0530

    Create a Mock Oracle Service

---
+++ MyInspection.Application/Interfaces/IOracleService.cs
@@ -0,0 +1,14 @@
+﻿using MyInspection.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInspection.Application.Interfaces
+{
+    public interface IOracleService
+    {
+        Task<PrefilledDataDto> GetPackingListDetailsAsync(string purchaseOrderNumber);
+    }
+}
---
+++ MyInspection.Infrastructure/Services/MockOracleService.cs
@@ -0,0 +1,33 @@
+﻿using MyInspection.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyInspection.Application.Interfaces;
+
+namespace MyInspection.Infrastructure.Services
+{
+    public class MockOracleService : IOracleService
+    {
+        public Task<PrefilledDataDto> GetPackingListDetailsAsync(string purchaseOrderNumber)
+        {
+            // Simulate a database lookup based on the PO number
+            // In a real app, this would query the Oracle DB.
+            if (purchaseOrderNumber == "25382020250411")
+            {
+                var data = new PrefilledDataDto
+                {
+                    SblOrderNumber = "E10223",
+                    ProductDescription = "AIRWALK DENVER 26IN",
+                    StyleNumber = "419-6941",
+                    TotalQuantity = 100
+                };
+                return Task.FromResult(data);
+            }
+
+            // Return default/empty data if PO not found
+            return Task.FromResult(new PrefilledDataDto());
+        }
+    }
+}
