d7764a4f7abcb51d3cc321ca18268dd1200968a7
Lasantha Karunarathne <lasantha.20223087@iit.ac.lk>
2025-09-01 08:37:43 +0530

    Define the Core Entities

---
+++ MyInspection.Core/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyInspection.Core.Entities
+{
+    // Extending IdentityRole gives us Id and Name
+    public class Role : IdentityRole<int> // Using 'int' for the primary key
+    {
+    }
+}
---
+++ MyInspection.Core/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyInspection.Core.Entities
+{
+    // Extending IdentityUser gives us Id, UserName, Email, PasswordHash, etc.
+    public class User : IdentityUser<int> // Using 'int' for the primary key
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
