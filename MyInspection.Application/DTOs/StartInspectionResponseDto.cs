4f91762ee614a6e9f72cd544ec6b280013b4be0b
Lasantha Karunarathne <lasantha.20223087@iit.ac.lk>
2025-09-01 10:56:50 +0530

    Create DTOs for the Inspection Report

---
+++ MyInspection.Application/DTOs/StartInspectionResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInspection.Application.DTOs
+{
+    public class StartInspectionResponseDto
+    {
+        public Guid ReportId { get; set; }
+        public PrefilledDataDto PrefilledData { get; set; }
+    }
+
+    public class PrefilledDataDto
+    {
+        public string SblOrderNumber { get; set; }
+        public string ProductDescription { get; set; }
+        public string StyleNumber { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
